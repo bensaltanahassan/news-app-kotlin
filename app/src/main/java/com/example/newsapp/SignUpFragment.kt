687e368279d038c767a77f25487f7c1ff36a8b64
Anas Hasna <hasnaanas230@gmail.com>
2023-12-14 16:23:34 +0100

    sign up post logic commit

--- app/src/main/java/com/example/newsapp/SignUpFragment.kt
+++ app/src/main/java/com/example/newsapp/SignUpFragment.kt
@@ -1,17 +1,23 @@
 package com.example.newsapp
 
+import Crud
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.newsapp.databinding.FragmentSignUpBinding
+import okhttp3.Call
+import okhttp3.Response
+import okio.IOException
+import org.json.JSONObject
 
 
 class SignUpFragment : Fragment() {
     private lateinit var _binding : FragmentSignUpBinding
     private val binding get() = _binding!!
-
+    val crud = Crud()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,8 +25,53 @@ class SignUpFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentSignUpBinding.inflate(inflater, container, false)
+        binding.signUpButton.setOnClickListener{
+            val firstName = binding.firstNameSignUp.text.toString()
+            val lastName = binding.lastNameSignUp.text.toString()
+            val email = binding.emailSignUp.text.toString()
+            val password = binding.passwordSignUp.text.toString()
+            val confirmPassword = binding.confirmPasswordSignUp.text.toString()
+            if(password == confirmPassword){
+                postLogin(firstName,lastName,email,password)
+            }else{
+                binding.errorTextSignUp.text="Les deux mot de passe ne se rassemblent pas"
+            }
 
+        }
         return binding.root
+    }
+
+    private fun postLogin(firstName : String ,lastName : String,email : String, password : String){
+        val loginUrl : String = "https://news-api-8kaq.onrender.com/api/auth/signup"
+        val json = """
+            {
+                "firstName": "$firstName",
+                "lastName": "$lastName",
+                "email": "$email",
+                "password": "$password"
+            }
+        """.trimIndent()
+
+
+        crud.post(loginUrl,json,object: Crud.ResponseCallback{
+            override fun onResponse(call: Call, response: Response) {
+                val responseData = response.body?.string()
+                val jsonresponse = JSONObject(responseData)
+                Log.d("POST Response:", "$responseData")
+                if(!response.isSuccessful){
+                    val message = jsonresponse.getString("message")
+                    requireActivity().runOnUiThread{
+                        binding.errorTextSignUp.text=message
+                    }
+
+                }
+            }
+            override fun onFailure(call: Call, e: IOException) {
+                Log.d("Request failed:", "${e.message}")
+
+            }
+        }
+        )
     }
 
 }
