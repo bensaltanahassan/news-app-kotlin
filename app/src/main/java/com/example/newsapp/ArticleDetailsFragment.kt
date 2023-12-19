56d739bea4127792ca2b2bd02e33370299d1aedc
Anas Hasna <hasnaanas230@gmail.com>
2023-12-19 16:28:21 +0100

    added appbar to news detail

--- app/src/main/java/com/example/newsapp/ArticleDetailsFragment.kt
+++ app/src/main/java/com/example/newsapp/ArticleDetailsFragment.kt
@@ -3,8 +3,11 @@ package com.example.newsapp
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.example.newsapp.databinding.FragmentArticleDetailsBinding
 
@@ -14,11 +17,18 @@ class ArticleDetailsFragment : Fragment() {
 
     private lateinit var toolbar : Toolbar
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_appbar_newsdetail, menu)
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentArticleDetailsBinding.inflate(inflater, container, false)
+        toolbar = binding.appbarNewsDetail.myToolBar
+        toolbar.title = "Article Details"
+        setHasOptionsMenu(true)
+        (activity as AppCompatActivity).setSupportActionBar(toolbar)
         val ratingBar = binding.ratingBar
         ratingBar.setOnRatingBarChangeListener { ratingBar, rating, fromUser ->
             ratingBar.rating.toInt()
